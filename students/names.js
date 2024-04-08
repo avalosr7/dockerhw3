300b2fd21e36b7bc5951a7899d95740e863cac65
avalosr7 <142945372+avalosr7@users.noreply.github.com>
2024-04-07 21:41:15 -0700

    Add files via upload

---
+++ students/names.js
@@ -0,0 +1,63 @@
+function loaddata(){
+	getname();
+	document.getElementById("babyselect").onchange = selectionMade;
+	getList();
+	getNameRankData();
+	getNameMeaning();
+}
+
+function getname() {
+    var select = document.getElementById("babyselect");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        
+        if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+            console.log("This request succeeded!", httpRequest.responseText);
+            var names = httpRequest.responseText.split("\n");
+            console.log("There are " + names.length + " names returned!");
+            
+            for(var i = 0; i < names.length; i++) {
+                var currentName = names[i];
+                var option = new Option(currentName, currentName);
+                select.add(option, false);
+                
+            }
+        }
+    }
+	httpRequest.open("GET", "http://localhost:8080/hw5/babynames.php", true);
+    httpRequest.send();
+}
+
+function selectionMade() {
+    var selectmade = document.getElementById("babyselect").value;
+    console.log("The selected name is: ", selectedName);
+	getList();
+    getNameMeaning();
+    getNameRankData();
+}
+
+function getList() {
+	console.log("Getting List");
+	httpRequest.open("GET", "http://localhost:8080/hw5/babynames.php?type=list", true);
+	httpRequest.send();
+}
+
+function getNameRankData() {
+	var select = document.getElementById("meaning").value;
+    console.log("Getting name rank data!");
+	httpRequest.open("GET", "http://localhost:8080/hw5/babynames.php?type=rank", true);
+    httpRequest.send();
+}
+
+function getNameMeaning() {
+	var getmean = document.getElementById("meaning");
+    console.log("Getting name meaning data!");
+	httpRequest.open("GET", "http://localhost:8080/hw5/babynames.php?type=meaning", true);
+    httpRequest.send();
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    runOnLoad();
+  }, false);
